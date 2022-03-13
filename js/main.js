0e097b7bb9069140397c3a83b04034af61627c00
wwdk <61912053+wdkkk@users.noreply.github.com>
2022-03-13 21:48:33 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const burger_arr = document.getElementsByClassName("burger__button_child");
+const nav_list = document.getElementById('nav_list');
+
+window.addEventListener('resize', function(e) {  
+    if (window.innerWidth <= 1200) {
+        nav_list.classList.add('__adaptive');
+    }
+    else nav_list.classList.remove('__adaptive');
+});
+
+
 function backToTop() {
 	let button = $('#to_top');
 
@@ -6,13 +17,53 @@ function backToTop() {
             e.preventDefault();
             $('html').animate({scrollTop: 0}, 1000);
         }
+
+        for (let i = 0; i <= 2; i++) {
+            burger_arr[i].classList.remove("__active");
+        }
+    
+        document.body.classList.remove('__fixed');
+    
+        nav_list.classList.remove('__adapitve');
+        nav_list.classList.remove('__active');
 	})
 };
 backToTop();
 
 window.addEventListener('scroll', function(e) {
     const nav = document.getElementById('nav');
-    
-    if (window.pageYOffset >= 50) nav.classList.add('__active');
-    else nav.classList.remove('__active')
+
+    if (window.pageYOffset >= 50) {
+        nav.classList.add('__active');
+        nav_list.classList.add('__blue');
+    } else {
+        nav.classList.remove('__active')
+        nav_list.classList.remove('__blue');
+    }
 });
+
+const burger = document.getElementById('burger_button');
+
+burger.onclick = function() {
+
+    for (let i = 0; i <= 2; i++) {
+        burger_arr[i].classList.toggle("__active");
+    }
+
+    document.body.classList.toggle('__fixed');
+
+    nav_list.classList.toggle('__adapitve');
+    nav_list.classList.toggle('__active');
+}
+
+$("#nav_list").on('click', '.nav__list__el', function(){
+    console.log('click')
+    for (let i = 0; i <= 2; i++) {
+        burger_arr[i].classList.remove("__active");
+    }
+
+    document.body.classList.remove('__fixed');
+
+    nav_list.classList.remove('__adapitve');
+    nav_list.classList.remove('__active');
+ });
